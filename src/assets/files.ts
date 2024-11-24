f0592256948b7db82a2d196432c3507bc2114006
blacktunes <blacktunes@foxmail.com>
2024-11-24 22:23:18 +0800

    fix: 修复打包后文件路径错误

--- src/assets/files.ts
+++ src/assets/files.ts
@@ -1,30 +1,31 @@
-const getUrl = (str: string) => new URL(str, import.meta.url).href
+const getAudioUrl = (name: string) => new URL(`./audio/${name}`, import.meta.url).href
+const getImageUrl = (name: string) => new URL(`./images/${name}`, import.meta.url).href
 
 export const AUDIO = [
   {
-    src: './audio/gululu.mp3',
+    src: 'gululu.mp3',
     weight: 5
   },
   {
-    src: './audio/gururu.mp3',
+    src: 'gururu.mp3',
     weight: 5
   },
   {
-    src: './audio/转圈圈.mp3',
+    src: '转圈圈.mp3',
     weight: 5
   },
   {
-    src: './audio/转圈圈咯.mp3',
+    src: '转圈圈咯.mp3',
     weight: 5
   },
   {
-    src: './audio/要坏掉了.mp3',
+    src: '要坏掉了.mp3',
     img: 1,
     weight: 1
   }
 ]
 
-export const IMAGES: [string, string] = ['./images/herta1.webp', './images/herta2.webp']
+export const IMAGES: [string, string] = ['herta1.webp', 'herta2.webp']
 
 export const progress: {
   raw: [number, number]
@@ -64,11 +65,11 @@ const preload = () => {
   const promises = []
 
   for (const i in AUDIO) {
-    AUDIO[i].src = getUrl(AUDIO[i].src)
+    AUDIO[i].src = getAudioUrl(AUDIO[i].src)
     promises.push(getObjectURL(AUDIO[i].src).then((result) => (AUDIO[i].src = result)))
   }
   for (const i in IMAGES) {
-    IMAGES[i] = getUrl(IMAGES[i])
+    IMAGES[i] = getImageUrl(IMAGES[i])
     promises.push(getObjectURL(IMAGES[i]).then((result) => (IMAGES[i] = result)))
   }
 
